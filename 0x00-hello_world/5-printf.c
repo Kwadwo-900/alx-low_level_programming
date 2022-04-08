39afc45ef4d49a0b1f17f9efd4c8675f35d87bc9
Kwadwo-900 <sagudze900@gmail.com>
2022-04-07 20:29:55 -0700

    task 5 added

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ *main- using the keyword printf to  output some text
+ *Return:0 wwhen program exits successfully and any number otherwise
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a pieceof art,");
+
+	return (0);
+}
