67cff5ddec05207edb8fc30330d16a79f5eac705
Kwadwo-900 <sagudze900@gmail.com>
2022-04-13 07:38:07 -0700

    file changes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,26 +2,22 @@
 
 /**
  * print_alphabet - prints alphabets to the stdout
- * main - working with alphabets
- * Return: 0 when successful
+ *
+ *
  */
 
-int main(void)
+
+void print_alphabet(void)
 {
-	void print_alphabet(void)
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
 	{
-		char ch;
-
-		ch = 'a';
-
-		while (ch <= 'z')
-		{
-			_putchar(ch);
-			ch++;
-		}
+		_putchar(ch);
+		ch++;
 	}
 	_putchar('\n');
-	return (0);
 }
-
 
