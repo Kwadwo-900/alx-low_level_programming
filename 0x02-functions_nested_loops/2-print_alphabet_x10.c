1d35e76de577f524b6128671fac55258dfa0a043
Kwadwo-900 <sagudze900@gmail.com>
2022-04-13 08:06:28 -0700

    file changes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *print_ alphabet_x10 - prints the alpahbets in lower case 10 times
+ *print_alphabet_x10 - prints the alpahbets in lower case 10 times
  */
 
 void print_alphabet_x10(void)
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 
 	i = 0;
 
-	while (i <= 10)
+	while (i < 10)
 	{
 		ch = 'a';
 
