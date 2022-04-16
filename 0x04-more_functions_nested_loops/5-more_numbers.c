466ac3326e068367356c4fbff9292e0d5c36f36b
Kwadwo-900 <sagudze900@gmail.com>
2022-04-16 05:45:05 -0700

    added task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*more_numbers - preints 10 times the numbes, from 0 to 14
+*followed by new line
+*Return: 1 on success, 0 otherwise
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 15; j++)
+		{
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
