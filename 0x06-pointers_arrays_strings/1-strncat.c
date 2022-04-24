8f8bc89797a0bcf004661f3e7641e066b46c68be
Kwadwo-900 <sagudze900@gmail.com>
2022-04-24 12:26:16 -0700

    file changes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -22,10 +22,10 @@ char *_strncat(char *dest, char *src, int n)
 
 	while (src[j] != '\0' && j <= n)
 	{
-		dest[i] = src[j];
+		dest[i + 1] = src[j];
 		i++;
 		j++;
 	}
-	dest[i] = '\0';
+	dest[i + 1] = '\0';
 	return (dest);
 }
